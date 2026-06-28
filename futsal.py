01aca11399c11f65ad5e821cac01df586d092141
nicolas-verdy <nicolas-verdy_student2024@wilder.school>
2026-06-28 16:14:20 +0200

    Update futsal.py

--- futsal.py
+++ futsal.py
@@ -66,7 +66,7 @@ st.subheader("RDV = 20H15      KickOff = 20H30")
 
 # attention :  nouvel horaire ::!!!!
 st.markdown(
-    "<div class='red-title'>Nouvel Horaire --> KickOff 20h30 RDV 20h15   (Yann rdv à 16h30 :o)</div>",
+    "<div class='red-title'>Nouvel Horaire --> KickOff 20h30 donc RDV 20h15 max</div>",
     unsafe_allow_html=True      )
 
 # attention :  nouvel horaire ::!!!!
