598be55f44b3cf92960b46889091a54b86eaaff3
nicolas-verdy <nicolas-verdy_student2024@wilder.school>
2026-06-28 16:05:03 +0200

    Update futsal.py

--- futsal.py
+++ futsal.py
@@ -58,7 +58,7 @@ st.title("⚽ Les Footix du Mardi en Juillet")
 st.subheader("RDV = 20H15      KickOff = 20H30")
 # attention :  nouvel horaire ::!!!!
 st.markdown(
-    "<div class='red-title'>ATTENTION Nouvel Horaire !! </div>",
+    "<div class='blue-title'>ATTENTION Nouvel Horaire !! </div>",
     unsafe_allow_html=True
 )
 
